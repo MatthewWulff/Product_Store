75984b2559ce2f9d95ddb2b90c71a065dd4592ef
Matthew <caspr117@gmail.com>
2025-11-04 13:46:03 -0500

    Frontend and Chakara import

---
+++ frontend/src/components/ui/provider.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ThemeProvider } from "next-themes"
+
+export function Provider({ children }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ChakraProvider value={defaultSystem}>
+        {children}
+      </ChakraProvider>
+    </ThemeProvider>
+  )
+}
