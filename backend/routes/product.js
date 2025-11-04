7e029243560a74505b04ede50aef9fd5f691ca60
Matthew <caspr117@gmail.com>
2025-11-04 10:44:21 -0500

    routes for route js optimization

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,66 +1,18 @@
 import express from "express"
 import Product from "../models/product.model.js"
 import mongoose from "mongoose"
+import { createProduct, deleteProduct, getProduct, updateProduct } from "../controllers/product.controller.js"
 
 const router = express.Router()
 
 
 
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find({})
-    res.status(200).json({ success: true, data: products })
-  } catch (error) {
-    console.log("Error", error.message)
-    res.status(500).json({ success: false, message: "Server Error" })
-  }
-})
+router.get("/", getProduct)
 
-router.post("/", async (req, res) => {
-  const product = req.body
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" })
-  }
-  const newProduct = new Product(product)
-  try {
-    await newProduct.save()
-    res.status(201).json({ success: true, data: newProduct })
-  } catch (error) {
-    console.error("Error in Creation", error.message)
-    res.status(500).json({ success: false, message: "Server Error" })
-  }
-})
+router.post("/", createProduct )
 
-router.put("/:id", async(req,res) => {
+router.put("/:id", updateProduct )
 
-    const {id} = req.params
-
-    const product = req.body
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).json({success: false, message: "Invalid Product Id "})
-    }
-
-    try{
-        const updatedProduct = await Product.findByIdAndUpdate(id,product,{new:true})
-        res.status(200).json({success: true , data: updatedProduct})
-    }catch(error){
-        res.status(500).json({success: false, message: "Server Error"})
-    }
-
-})
-
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params
-  try {
-    await Product.findByIdAndDelete(id)
-    res.status(200).json({ success: true, message: "Product Deleted" })
-  } catch (error) {
-    console.log("Error in deleting product")
-    res.status(404).json({ success: false, message: "Product not found" })
-  }
-})
+router.delete("/:id", deleteProduct)
 
 export default router
