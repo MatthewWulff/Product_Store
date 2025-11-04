72364dae5fdbca81fba7e662f254f45f92bf63e4
Matthew <caspr117@gmail.com>
2025-11-04 10:28:15 -0500

    More routes and route allocation

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js"
+import productRoutes from "./routes/product.js"
+
+
+
+
+
 
 dotenv.config()
 
-
 const app = express()
 
-console.log(process.env.MONGO_URI)
+app.use(express.json())
 
-app.post("/products",(req,res) => {
-    res.send("Server is ready")
-})
+app.use("/api/products",productRoutes)
 
-app.listen(5000,()=>{
-    connectDB()
-    console.log("Server started at http://localhost:5000 hello")
+
+app.listen(5000, () => {
+  connectDB()
+  console.log("Server started at http://localhost:5000 hello")
 })
