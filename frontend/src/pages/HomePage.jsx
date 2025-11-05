3c08c301c38d85792e45e6bdb2c5d239f2863461
Matthew <caspr117@gmail.com>
2025-11-05 16:54:24 -0500

    Homepage defualt with no products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,37 @@
+import { Container, VStack,Text } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW = 'container.xl' py ={12}>
+      <VStack spacing = {8}>
+        <Text
+        fontSize= {"30"}
+        fontWeight={"bold"}
+        bgGradient= {"linear(to-r, cyan.400, blue.500)"}
+        bgClip = {"text"}
+        textAlign = {"Center"}>
+          Current Products
+        </Text>
+
+        <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+						No products found 😢{" "}
+            
+						<Link to={"/create"}>
+							<Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
+								Create a product
+							</Text>
+						</Link>
+					</Text>
+
+
+      
+     
+      </VStack>
+
+
+    </Container>
   )
 }
 
