d5c614f2dfc60283269284eb6ff93734d21f449b
Matthew <caspr117@gmail.com>
2025-11-04 19:29:40 -0500

    Nav bar fix and light?dark mode added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import { Box } from "@chakra-ui/react";
-import { Route, Routes } from "react-router-dom";
-import CreatePage from "./pages/CreatePage";
-import HomePage from "./pages/HomePage";
-import NavBar from "./components/NavBar";
+import { Box, useColorModeValue } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+
+import CreatePage from "./pages/CreatePage"
+import HomePage from "./pages/HomePage"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <Box bg="gray.100" minH="100vh" color="gray.800">
-      <NavBar />
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
       </Routes>
     </Box>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Provider } from "./components/ui/provider.jsx"; 
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<ChakraProvider>
+				<App />
+			</ChakraProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
